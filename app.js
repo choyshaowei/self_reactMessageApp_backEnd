0c0dd7d1b4d1dfb2ec3416e696f179a65a87cb48
Shao Wei <33794625+choyshaowei@users.noreply.github.com>
2021-01-30 18:35:32 +0800

    Enable GrapQL API

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const { graphqlHTTP } = require("express-graphql");
 const { v4: uuidv4 } = require("uuid");
 
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
 
 const app = express();
 
@@ -45,16 +46,33 @@ app.use((req, res, next) => {
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
-app.use("/auth", authRoutes);
-app.use("/feed", feedRoutes);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    customFormatErrorFn(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An Error Occured.";
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
-  const message = error.message;
   const data = error.data;
   res.status(status).json({ message: message, data: data });
 });
@@ -65,11 +83,7 @@ mongoose
     (option = { useNewUrlParser: true, useUnifiedTopology: true })
   )
   .then((result) => {
-    const server = app.listen(8080);
-    const io = require("./socket").init(server);
-    io.on("connection", (socket) => {
-      console.log("Client Connected");
-    });
+    app.listen(8080);
     console.log("Server started");
   })
   .catch((err) => console.log(err));
